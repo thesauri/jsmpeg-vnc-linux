f780274b83684403b6b47b9047eacee9634108af
Walter Berggren <walter.berggren@gmail.com>
2016-06-22 10:03:40 +0200

    Potentially working implementation of grabber_grab

--- src/grabber.c
+++ src/grabber.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <X11/Xutil.h>
 
 #include "grabber.h"
 
@@ -16,12 +17,30 @@ grabber_t *grabber_create(Display *display, Window window) {
   self->display = display;
   self->window = window;
 
+  self->pixels_size = self->width * self->height * 4;
+  self->pixels = malloc(self->pixels_size);
+
   return self;
 }
 
 void grabber_destroy(grabber_t *self) {
   if (self != NULL) {
-    //free(self->pixels);
+    free(self->pixels);
     free(self);
   }
+}
+
+void *grabber_grab(grabber_t *self) {
+  XImage *image = XGetImage(self->display,
+                            self->window,
+                            0, 0,
+                            self->width, self->height,
+                            AllPlanes,
+                            ZPixmap);
+
+  memcpy(self->pixels, image->data, self->pixels_size);
+
+  XDestroyImage(image);
+
+  return self->pixels;
 }
--- src/grabber.h
+++ src/grabber.h
@@ -8,7 +8,9 @@ typedef struct {
 	int height;
 
 	void *pixels;
+  int pixels_size;
 } grabber_t;
 
 grabber_t *grabber_create(Display *display, Window window);
 void grabber_destroy(grabber_t *self);
+void *grabber_grab(grabber_t *self);
