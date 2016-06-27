58ef3397ab87e45587fb52b36bad4e7a79ba3749
Walter <walter.berggren@gmail.com>
2016-06-27 11:42:59 +0200

    Finish port

--- src/server.c
+++ src/server.c
@@ -1,8 +1,11 @@
+#define _BSD_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #include "server.h"
 
@@ -47,4 +50,142 @@ server_t *server_create(int port, size_t buffer_size) {
 	self->clients = NULL;
 
 	lws_set_log_level(LLL_ERR | LLL_WARN, NULL);
+
+  struct lws_context_creation_info info = {0};
+	info.port = port;
+	info.gid = -1;
+	info.uid = -1;
+	info.user = (void *)self;
+	info.protocols = server_protocols;
+	self->context = libwebsocket_create_context(&info);
+
+  if( !self->context ) {
+		server_destroy(self);
+		return NULL;
+	}
+
+	return self;
+}
+
+void server_destroy(server_t *self) {
+	if( self == NULL ) { return; }
+
+	if( self->context ) {
+		libwebsocket_context_destroy(self->context);
+	}
+
+	free(self->send_buffer_with_padding);
+	free(self);
+}
+
+char *server_get_host_address(server_t *self) {
+	char host_name[80];
+	struct hostent *host;
+	if(
+		gethostname(host_name, sizeof(host_name)) == -1 ||
+		!(host = gethostbyname(host_name))
+	) {
+		return "127.0.0.1";
+	}
+
+	return inet_ntoa( *(struct in_addr *)(host->h_addr_list[0]) );
+}
+
+char *server_get_client_address(server_t *self, struct libwebsocket *wsi) {
+	static char ip_buffer[32];
+	static char name_buffer[32];
+
+	libwebsockets_get_peer_addresses(
+		self->context, wsi, libwebsocket_get_socket_fd(wsi),
+		name_buffer, sizeof(name_buffer),
+		ip_buffer, sizeof(ip_buffer)
+	);
+	return ip_buffer;
+}
+
+void server_update(server_t *self) {
+	libwebsocket_callback_on_writable_all_protocol(&(server_protocols[1]));
+	libwebsocket_service(self->context, 0);
+}
+
+void server_send(server_t *self, struct libwebsocket *socket, void *data, size_t size, server_data_type_t type) {
+	// Caution, this may explode! The libwebsocket docs advise against ever calling libwebsocket_write()
+	// outside of LWS_CALLBACK_SERVER_WRITEABLE. Honoring this advise would complicate things quite
+	// a bit - and it seems to work just fine on my system as it is anyway.
+	// This won't work reliably on mobile systems where network buffers are typically much smaller.
+	// ¯\_(ツ)_/¯
+
+	if( size > self->buffer_size ) {
+		printf("Cant send %zu bytes; exceeds buffer size (%zu bytes)\n", size, self->buffer_size);
+		return;
+	}
+	memcpy(self->send_buffer, data, size);
+	libwebsocket_write(socket, self->send_buffer, size, (enum libwebsocket_write_protocol)type);
+}
+
+void server_broadcast(server_t *self, void *data, size_t size, server_data_type_t type) {
+	if( size > self->buffer_size ) {
+		printf("Cant send %zu bytes; exceeds buffer size (%zu bytes)\n", size, self->buffer_size);
+		return;
+	}
+	memcpy(self->send_buffer, data, size);
+
+	client_foreach(self->clients, client) {
+		libwebsocket_write(client->socket, self->send_buffer, size, (enum libwebsocket_write_protocol)type);
+	}
+	libwebsocket_callback_on_writable_all_protocol(&(server_protocols[1]));
+}
+
+static int callback_websockets(
+	struct libwebsocket_context *context,
+	struct libwebsocket *wsi,
+	enum libwebsocket_callback_reasons reason,
+	void *user, void *in, size_t len
+) {
+	server_t *self = (server_t *)libwebsocket_context_user(context);
+
+	switch( reason ) {
+		case LWS_CALLBACK_ESTABLISHED:
+			client_insert(&self->clients, wsi);
+
+			if( self->on_connect ) {
+				self->on_connect(self, wsi);
+			}
+			break;
+
+		case LWS_CALLBACK_RECEIVE:
+			if( self->on_message ) {
+				self->on_message(self, wsi, in, len);
+			}
+			break;
+
+		case LWS_CALLBACK_CLOSED:
+			client_remove(&self->clients, wsi);
+
+			if( self->on_close ) {
+				self->on_close(self, wsi);
+			}
+			break;
+	}
+
+	return 0;
+}
+
+static int callback_http(
+	struct libwebsocket_context *context,
+	struct libwebsocket *wsi,
+	enum libwebsocket_callback_reasons reason, void *user,
+	void *in, size_t len
+) {
+	server_t *self = (server_t *)libwebsocket_context_user(context);
+
+	if( reason == LWS_CALLBACK_HTTP ) {
+		if( self->on_http_req && self->on_http_req(self, wsi, (char *)in) ) {
+			return 0;
+		}
+		libwebsockets_return_http_status(context, wsi, HTTP_STATUS_NOT_FOUND, NULL);
+		return 0;
+	}
+
+	return 0;
 }
