457f25851c205657228394447f84e97d2cf44696
Walter Berggren <walter.berggren@gmail.com>
2016-06-27 17:01:03 +0200

    Uncomment on_message processing, but remove all windows-specific sending of events

--- src/app.c
+++ src/app.c
@@ -140,58 +140,31 @@ void app_on_close(app_t *self, struct libwebsocket *socket) {
 void app_on_message(app_t *self, struct libwebsocket *socket, void *data, size_t len) {
 	input_type_t type = (input_type_t)((unsigned short *)data)[0];
 
-	/*if( type & input_type_key && len >= sizeof(input_key_t) ) {
+	if( type & input_type_key && len >= sizeof(input_key_t) ) {
 		input_key_t *input = (input_key_t *)data;
 
-		if( input->key_code == VK_CAPITAL ) { return; } // ignore caps lock
+		Window winFocus;
+		int revert;
+		XGetInputFocus(display, &winfocus, &revert);
 
-		UINT scan_code = MapVirtualKey(input->key_code, MAPVK_VK_TO_VSC_EX);
-		UINT flags = KEYEVENTF_SCANCODE	| (input->state ? 0 : KEYEVENTF_KEYUP);
+		
 
-		// set extended bit for some keys
-		switch( input->key_code ) {
-			case VK_LEFT: case VK_UP: case VK_RIGHT: case VK_DOWN:
-			case VK_PRIOR: case VK_NEXT: case VK_END: case VK_HOME:
-			case VK_INSERT: case VK_DELETE: case VK_DIVIDE: case VK_NUMLOCK:
-				scan_code |= 0x100;
-				flags |= KEYEVENTF_EXTENDEDKEY;
-				break;
-		}
-
-		//printf("key: %d -> %d\n", input->key_code, scan_code);
-		keybd_event((BYTE)input->key_code, scan_code, flags, 0);
 	}
 	else if( type & input_type_mouse && len >= sizeof(input_mouse_t) ) {
 		input_mouse_t *input = (input_mouse_t *)data;
 
 		if( type & input_type_mouse_absolute ) {
-			POINT window_pos = {0, 0};
-			ClientToScreen(self->grabber->window, &window_pos);
 
-			// figure out the x / y scaling for the rendered video
-			float scale_x = ((float)self->encoder->in_width / self->encoder->out_width),
-				scale_y =((float)self->encoder->in_height / self->encoder->out_height);
-
-			int x = (int)(input->x * scale_x + window_pos.x),
-				y = (int)(input->y * scale_y + window_pos.y);
-
-			//printf("mouse absolute %d, %d\n", x, y);
-			SetCursorPos(x, y);
 		}
 
 		if( type & input_type_mouse_relative ) {
-			int x = (int)(input->x * self->mouse_speed),
-				y = (int)(input->y * self->mouse_speed);
 
-			//printf("mouse relative %d, %d\n", x, y);
-			mouse_event(MOUSEEVENTF_MOVE, x, y, 0, NULL);
 		}
 
 		if( type & input_type_mouse_button ) {
-			//printf("mouse button %d\n", input->flags);
-			mouse_event(input->flags, 0, 0, 0, NULL);
+
 		}
-	}*/
+	}
 }
 
 void app_run(app_t *self, int target_fps) {
@@ -244,4 +217,29 @@ void app_run(app_t *self, int target_fps) {
 
 double time_since(clock_t start) {
   return (clock() - start) * 1000 / CLOCKS_PER_SEC;
+}
+
+XKeyEvent createKeyEvent(Display *display, Window win, Window winRoot, _Bool press, int keycode, int modifiers) {
+	XKeyEvent event;
+
+	event.display     = display;
+	event.window      = win;
+	event.root        = winRoot;
+	event.subwindow   = None;
+	event.time        = CurrentTime;
+	event.x           = 1;
+	event.y           = 1;
+	event.x_root      = 1;
+	event.y_root      = 1;
+	event.same_screen = True;
+	event.keycode     = XKeysymToKeycode(display, keycode);
+	event.state       = modifiers;
+
+	if (press) {
+		event.type = KeyPress;
+	} else {
+		event.type = KeyRelease;
+	}
+
+	return event;
 }
